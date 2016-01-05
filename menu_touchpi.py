5e542c4adee4979aa0697db928abd7f0c1fba0cf
stuart.stent <stuart.stent@gmail.com>
2016-01-04 19:10:38 -0500

    random

--- menu_touchpi.py
+++ menu_touchpi.py
@@ -37,12 +37,12 @@ def on_touch():
     touch_pos = (pygame.mouse.get_pos() [0], pygame.mouse.get_pos() [1])
     #  x_min                 x_max   y_min                y_max
     map_val = screen_click_map[touch_pos[0]][touch_pos[1]]
-    if map_val == None:
-        print "Not a button!!"
-    else:
-        pprint (map_val)
+    # if map_val == None:
+    #     print "Not a button!!"
+    # else:
+    #     pprint (map_val)
         #button(map_val)
-
+    pprint (map_val)
 
     # # button 1 event
     # if 30 <= touch_pos[0] <= 240 and 105 <= touch_pos[1] <=160:
